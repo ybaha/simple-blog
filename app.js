883d98f56c6f514fa5fa342a061f83ecf06db1f8
ybaha <ybahaerarslan@gmail.com>
2020-03-01 02:53:01 +0300

    added slide-bar animation

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ function sliderLoadFunc(){
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
     document.getElementById("hr").style.width = scrolled + "%";
-    console.log(winScroll);
     
+    if(winScroll == 0){
+        document.getElementById("hr").style.animationName = "div-slider";
+    }
+    else{
+        document.getElementById("hr").style.animationName = null;
+    }
 }
 
 
