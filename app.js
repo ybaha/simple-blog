9827d71237ffe791edd7e7663c24d241e6a58547
ybaha <ybahaerarslan@gmail.com>
2020-03-01 18:57:18 +0300

    navbar animation

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ window.onscroll = function() {sliderLoadFunc()};
 function sliderLoadFunc(){
     var winScroll =  document.documentElement.scrollTop;;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("hr").style.width = scrolled + "%";
+    var scrolled = (winScroll / height) * 50;
+    document.getElementById("hr").style.width = 50-scrolled + "%";
+    document.getElementById("hr2").style.width = 50-scrolled + "%";
     
-    if(winScroll == 0){
+    if(winScroll == height){
         document.getElementById("hr").style.animationName = "div-slider";
+        document.getElementById("hr2").style.animationName = "div-slider";
     }
     else{
         document.getElementById("hr").style.animationName = null;
+        document.getElementById("hr2").style.animationName = null;
     }
 }
+
+    
+
 
 
 
